ff8fc3c15a180a5e41c859419cf88f0a57878d7a
Luís Conceição <luis.conceicao@fabamaq.com>
2023-11-25 12:04:15 +0000

    Adding configuration for install and layout.

---
+++ export/bin/app.genisys.lua
@@ -0,0 +1,14 @@
+local genisys = require("genisys")
+
+local game = genisys.create_application("game")
+
+game.name = "Croccantini Game Bundle"
+game.version = "0.0.6"
+game.fullscreen = true
+game.description = "First bundle to test monitors layout."
+game.thumbnail = genisys.get_path("thumbnail.png")
+
+local launcher = game:create_process()
+launcher.command = { genisys.get_path("launcher.x86_64"), "--rendering-driver", "opengl3"}
+
+return game
