68c2852fb3c2cec916d7ebcc953a2a3912c571e3
Filipe Costa <filipe.costa@fabamaq.com>
2023-11-25 15:50:09 +0000

    Added Gameplay Arena (main game scene)

---
+++ src/gameplay/Gameplay Arena.gd
@@ -0,0 +1,26 @@
+extends Node3D
+
+@onready var _player = $Player
+@onready var _camera = $Player/Camera3D
+
+@onready var _enemy = $"Enemy Placeholder"
+@onready var _arena = $"Arena Placeholder"
+
+const rotate_speed = 6
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	_player.gameplay_arena = self
+
+
+var _target_arena_rotation = 0.0
+func rotate_arena(clockwise):
+	if clockwise:
+		_target_arena_rotation += PI * 2 / 13
+	else:
+		_target_arena_rotation -= PI * 2 / 13
+	
+
+
+func _process(delta):
+	_arena.rotation = _arena.rotation.lerp(Vector3(0, _target_arena_rotation, 0), delta * rotate_speed)
