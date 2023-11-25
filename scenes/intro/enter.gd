ac57ad761828bd0a1f61c67d904606c33f2cee7b
Luís Conceição <luis.conceicao@fabamaq.com>
2023-11-25 21:22:22 +0000

    Fixing filter and screen resolutions.

--- scenes/intro/enter.gd
+++ scenes/intro/enter.gd
@@ -2,6 +2,7 @@ extends Control
 
 @onready var brand_label: Label = $ColorRect/VBoxContainer/Brand/CenterContainer/Label
 @onready var play_label: Label = $ColorRect/VBoxContainer/Info/PlayLabelContainer/CenterContainer/Label
+@onready var label_res: Label = $ColorRect/VBoxContainer/Info/TutorialLabelContainer/CenterContainer/Label2
 
 var visible_count: int = 0
 
@@ -11,3 +12,6 @@ func _on_timer_timeout():
 		return
 	play_label.visible = not play_label.visible
 	visible_count = 0
+
+func _ready():
+	label_res.text = str(DisplayServer.screen_get_size().x)  +"-" + str(DisplayServer.screen_get_size().y) 
